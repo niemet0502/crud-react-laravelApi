0085245fb0c4ac6aa37facbb4460b01bf6d05ab9
niemet0502 <vincentmarius8@gmail.com>
2020-10-24 03:44:18 +0200

    affichage de la liste des actualites sur la page d'accueil

---
+++ front/src/App.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from 'react';
+import Axios from "axios"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  const api_url = 'http://laravel-api.test/api/topicality';
+  const [topicalities, setTopicalities] = useState([]);
+
+  function topicaliti(){
+    Axios.get(api_url)
+    .then(response =>{
+      const res = response.data
+      console.log(res);
+      setTopicalities(res);
+    })
+  }
+
+  useEffect(() => {
+    topicaliti();
+  }, []);
+
+  function truncate(str, n) {
+    return str?.lenght > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <div className="App">
+        <h1 className="text-center">Liste des actualites</h1>
+        <table class="table col-md-10 offset-1">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">TITRE</th>
+              <th scope="col">CONTENUE</th>
+              <th scope="col">ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+              {topicalities.map((topicality) => (
+                <tr>
+                  <td>
+                    <h6>{topicality.title}</h6>
+                  </td>
+                  <td>
+                    <p>
+                      {truncate(topicality.content, 60)}
+                    </p>
+                  </td>
+                  <td>
+                    <button className="btn btn-warning">EDITER</button>
+                    <button className="btn btn-danger ml-4">SUPPRIMER</button>
+                  </td>
+                </tr>
+                
+              ))}
+          </tbody>
+         </table>
+    </div>
+  );
+}
+
+export default App;
